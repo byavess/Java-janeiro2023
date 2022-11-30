bff71d9334f974127f18955f7d61db093c614b8e
Sueliton de Oliveira <sueliton.oliveira@petzpartner.com.br>
2022-11-30 17:41:32 -0300

    orientação

---
+++ src/OrientadaObjeto/ExercicosRetangulo.java
@@ -0,0 +1,9 @@
+package OrientadaObjeto;
+
+public class ExercicosRetangulo {
+    public double Width;
+    public double Heigth;
+    public double area () {
+        return Width*Heigth;
+    }
+}
